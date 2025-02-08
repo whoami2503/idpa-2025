3859ce1bf384d62615ae18604cb0aa092ff938f0
Ilian Häsler <ilianhaesler@gmail.com>
2025-02-08 15:54:01 +0100

    feat: cookies modal

---
+++ scripts/cookies.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieModal = document.getElementById("cookie-modal");
+
+    // Check if user has already accepted cookies
+    if (localStorage.getItem("cookieAccept") === "true") {
+        cookieModal.classList.add("hidden");
+    } else {
+        cookieModal.classList.remove("hidden");
+    }
+});
+
+function setCookieAccept() {
+    localStorage.setItem("cookieAccept", "true");
+
+    // Hide the modal
+    document.getElementById("cookie-modal").classList.add("hidden");
+}
